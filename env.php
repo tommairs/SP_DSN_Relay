8b8c10e07560d12cd61df34e2fbe4f07a130b4d2
Tom Mairs <tom.mairs@messagesystems.com>
2019-08-26 11:47:17 -0400

    Create env.php

---
+++ env.php
@@ -0,0 +1,7 @@
+<?php
+// This file should be updated to your specific needs AFTER implementation.
+// DO NOT commit changes to this file with the username and password edited.
+$TZ = "America/Los_Angeles";
+$whcuser = "<redacted>";
+$whcpass = "<redacted>";
+?>
